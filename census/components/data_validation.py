4d51921055a9e047f733f6caf95d522da3a1f5b4
mohitsandhuria <ymohitsandhuria@gmail.com>
2023-01-15 16:57:31 +0530

    completed data validation

--- census/components/data_validation.py
+++ census/components/data_validation.py
@@ -61,7 +61,8 @@ class DataValidation:
         except Exception as e:
             raise CensusException(e, sys)
         
-        def data_drift(self,current_df,base_df,report_key_name):
+    def data_drift(self,current_df,base_df,report_key_name):
+        try:
             drift_report=dict()
 
             base_columns=base_df.columns
@@ -73,9 +74,66 @@ class DataValidation:
                 logging.info(f"Hypothesis {base_column}: {base_data.dtype}, {current_data.dtype} ")
                 same_distribution =ks_2samp(base_data,current_data)
             
-                if same_distribution.p_value>0.5:
-                    drift_report
+                if same_distribution.pvalue>0.05:
+                    drift_report={"p_value":float(same_distribution.pvalue),"same_distribution":True}
+                else:
+                    drift_report={"p_value":float(same_distribution.pvalue),"same_distribution":False}
+                self.validation_error=drift_report
+        except Exception as e:
+            raise CensusException(e,sys)
 
+
+    def initiate_data_validation(self):
+        try:
+            logging.info(f"Reading base file to form data frame")
+            base_df=pd.read_csv(self.data_ingestion_artifact.feature_store_file_path)
+            logging.info(f"Reading train and test file from artefacts")
+            cat_cols=[var for var in base_df.columns if base_df[var].dtype=='O']
+            num_cols=[var for var in base_df.columns if base_df[var].dtype!='O']
+            for i in cat_cols:
+                base_df[i]=base_df[i].str.strip().map(lambda x: np.nan if x=="?" else x)
+            logging.info("dropping null values columns from base dataset")
+            base_df=self.drop_missing_values_columns(df=base_df, report_key_name="missing_values_in_base_dataset")
+
+            logging.info(f"Reading Train dataset")
+            train_df=pd.read_csv(self.data_ingestion_artifact.train_file_path)
+            logging.info(f"Reading Train dataset")
+            test_df=pd.read_csv(self.data_ingestion_artifact.test_file_path)
+            logging.info("dropping null values columns from train dataset")
+            train_df=self.drop_missing_values_columns(df=train_df, report_key_name="missing_values_in_train_dataset")
+            logging.info("dropping null values columns from test dataset")
+            test_df=self.drop_missing_values_columns(df=test_df, report_key_name="missing_values_in_test_dataset")
+
+            exclude_column=cat_cols
+            base_df=utils.convert_column_to_float(df=base_df, exclude_column=exclude_column)
+            train_df=utils.convert_column_to_float(df=train_df, exclude_column=exclude_column)
+            test_df=utils.convert_column_to_float(df=test_df, exclude_column=exclude_column)
+
+            base_df=base_df[num_cols]
+            train_df=train_df[num_cols]
+            test_df=test_df[num_cols]
+
+
+            logging.info("Checking if required column exists in train and test df")
+
+            train_df_column_status=self.is_required_column_exists(base_df=base_df, current_df=train_df, report_key_name="missing_columns_within_train_dataset")
+            test_df_column_status=self.is_required_column_exists(base_df=base_df, current_df=test_df, report_key_name="missing_columns_within_train_dataset")
+
+            if train_df_column_status:
+                logging.info(f"As all column are available in train df hence detecting data drift")
+                self.data_drift(current_df=train_df, base_df=base_df, report_key_name="data_drift_within_train_dataset")
+            if test_df_column_status:
+                logging.info(f"As all column are available in test df hence detecting data drift")
+                self.data_drift(current_df=test_df, base_df=base_df, report_key_name="data_drift_within_test_dataset")
+            
+            logging.info("Writting report to yml")
+            utils.write_to_yaml(file_path=self.data_validation_config.report_file_path, data=self.validation_error)
+
+            data_validation_artifact=artifact_entity.DataValidationArtifact(report_file_path=self.data_validation_config.report_file_path)
+            logging.info(f"Data validation artifact: {data_validation_artifact}")
+            return data_validation_artifact
+        except Exception as e:
+            raise CensusException(e, sys)    
 
 
 
--- census/utils/__init__.py
+++ census/utils/__init__.py
@@ -5,6 +5,7 @@ import numpy as np
 import sys
 import os
 from census.config import mongo_client
+import yaml
 
 def get_collection_dataframe(database_name,collection_name):
 
@@ -17,5 +18,25 @@ def get_collection_dataframe(database_name,collection_name):
             df.drop('_id',axis=1,inplace=True)
         logging.info(f"Number of rows and columns in dataframe {df.shape}")
         return df
+    except Exception as e:
+        raise CensusException(e, sys)
+
+def write_to_yaml(file_path,data):
+    try:
+        file_dir=os.path.dirname(file_path)
+        os.makedirs(file_dir,exist_ok=True)
+
+        with open(file_path,'w') as file_writer:
+            yaml.dump(data,file_writer)
+    except Exception as e:
+        raise CensusException(e, sys)
+
+def convert_column_to_float(df,exclude_column):
+    try:
+        for column in df.columns:
+            if column not in exclude_column:
+                df[column]=df[column].astype('float')
+        return df
+
     except Exception as e:
         raise CensusException(e, sys)
